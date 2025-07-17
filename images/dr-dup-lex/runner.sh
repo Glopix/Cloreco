f113776ff66d18691d15370f194fde3d436bee1d
Glopix <glopix.glopix@gmail.com>
2025-07-17 09:42:07 +0200

    add DrDupLex

---
+++ images/dr-dup-lex/runner.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# runner script for DrDupLex (https://github.com/tronicek/DrDupLex)
+
+CLONES_INPUT_PATH="$1"
+
+# dir of the tool
+MAIN_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+cd "${MAIN_DIR}"
+
+# Check if LOGGING_VERBOSE env var is set and not empty
+if [[ -n "$LOGGING_VERBOSE" ]]; then
+  OUTPUT_TARGET="$LOGGING_VERBOSE"
+else
+  # if it is not set, redirects both stdout and stderr of all commands to /dev/null
+  OUTPUT_TARGET="/dev/null"
+fi
+
+CONFIG_FILE_ORIG="${MAIN_DIR}/drDupLex-mounted.config"
+CONFIG_FILE="${MAIN_DIR}/drDupLex.config"
+KEY="sourceDir"
+
+# since the config file is mounted, it can't be changed in place
+# which is why we copy it
+cp "$CONFIG_FILE_ORIG" "$CONFIG_FILE"
+
+# Check if the 'sourceDir' key exists and set it to the clones path, or append if it doesn't
+if grep -q "^${KEY}[[:space:]]*="   "$CONFIG_FILE"; then
+    sed -i "s|^${KEY}[[:space:]]*=.*|${KEY}=${CLONES_INPUT_PATH}|" "$CONFIG_FILE"
+else
+    echo "${KEY}=${CLONES_INPUT_PATH}" >> "$CONFIG_FILE"
+fi
+
+# execute DrDupLex-1.0 with drdup.config configuration file,
+# redirect and append both stdout and stderr to $OUTPUT_TARGET
+java -jar target/DrDupLex-1.0-jar-with-dependencies.jar "$CONFIG_FILE"  &>> $OUTPUT_TARGET    || exit 1
+
+python3 transform-output-xml-to-csv.py drdup-output.xml drdup-output.csv
+
+# print results to stdout
+cat drdup-output.csv
+
+rm drdup-output.xml drdup-output.csv
